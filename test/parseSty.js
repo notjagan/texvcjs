13c86b0d98eb47e72e11bb2cf5463a12a517cf94
Jagan Prem <notjagan@gmail.com>
2016-08-09 14:54:26 -0400

    Add test for findBracketSections in parseSty.js.

--- test/parseSty.js
+++ test/parseSty.js
@@ -7,20 +7,30 @@ var path = require("path");
 
 var testCases = [
     {
+        input: "\\newcommand{\\test}{\\Te}[0]",
+        output: [
+            ["\\test", "{"],
+            ["\\Te", "{"],
+            ["0", "["]
+        ],
+        func: parseSty.findBracketSections
+    },
+    {
         input: fs.readFileSync(path.join(__dirname, 'parseSty.sty'), 'utf8'),
         output: {
             "\\testA": "\\TeA",
             "\\testB": "\\TeB",
             "\\testC": "\\TeC",
             "\\testD": "\\mbox{\\TeD}"
-        }
+        },
+        func: parseSty.parseSty
     }
 ];
 
 describe('ParseSty', function() {
     testCases.forEach(function(tc) {
         it('should identify commands in \n' + tc.input.replace(/^/gm, '\t'), function() {
-            assert.deepEqual(parseSty(tc.input), tc.output);
+            assert.deepEqual(tc.func(tc.input), tc.output);
         });
     });
 });
