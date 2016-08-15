376222650ea76467ac6e8f52330890ef2068d65a
Jagan Prem <notjagan@gmail.com>
2016-08-15 11:10:54 -0400

    Add tests and improve comment style for parseSty.js

--- test/parseSty.js
+++ test/parseSty.js
@@ -7,30 +7,49 @@ var path = require("path");
 
 var testCases = [
     {
-        input: "\\newcommand{\\test}{\\Te}[0]",
+        func: parseSty.findBracketSections,
+        input: [ "\\newcommand{\\test}{\\Te}[0]" ],
         output: [
             ["\\test", "{"],
             ["\\Te", "{"],
             ["0", "["]
-        ],
-        func: parseSty.findBracketSections
+        ]
     },
     {
-        input: fs.readFileSync(path.join(__dirname, 'parseSty.sty'), 'utf8'),
+        func: parseSty.findFirst,
+        input: [
+            "\\newcommand\\DeclareRobustCommand", [ "\\newcommand", "\\DeclareRobustCommand" ], 0
+        ],
+        output: [ 0, "\\newcommand"]
+    },
+    {
+        func: parseSty.findFirst,
+        input: [
+            "\\newcommand\\DeclareRobustCommand", [ "\\newcommand", "\\DeclareRobustCommand" ], 2
+        ],
+        output: [ 11, "\\DeclareRobustCommand"]
+    },
+    {
+        func: parseSty.bracketBalance,
+        input: [ "(( test ) [" ],
+        output: 2
+    },
+    {
+        func: parseSty.parseSty,
+        input: [ fs.readFileSync(path.join(__dirname, 'parseSty.sty'), 'utf8') ],
         output: {
             "\\testA": "\\TeA",
             "\\testB": "\\TeB",
             "\\testC": "\\TeC",
             "\\testD": "\\mbox{\\TeD}"
-        },
-        func: parseSty.parseSty
+        }
     }
 ];
 
 describe('ParseSty', function() {
     testCases.forEach(function(tc) {
-        it('should identify commands in \n' + tc.input.replace(/^/gm, '\t'), function() {
-            assert.deepEqual(tc.func(tc.input), tc.output);
+        it(JSON.stringify(tc.input).replace(/^/g, "\t"), function() {
+            assert.deepEqual(tc.func.apply(this, tc.input), tc.output);
         });
     });
 });
